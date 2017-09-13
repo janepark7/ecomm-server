6ad75d3d5dc46fb05d5ca994fc55385f33d9a031
Jane <janeparkjp@gmail.com>
2017-09-12 21:56:50 -0400

    change in import checkoutform

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,4 +1,4 @@
-import "./checkoutForm.scss";
+import "./CheckoutForm.scss";
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { submitOrder } from "actions/checkout";
